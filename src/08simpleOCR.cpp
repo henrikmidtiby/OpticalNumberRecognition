b39a5ad252b58bd572e0b02cc5f7d36ccd1d13b8
Henrik Skov Midtiby <hemi@mmmi.sdu.dk>
2016-10-13 13:59:12 +0200

    It works! But with a lot of errors.

--- src/08simpleOCR.cpp
+++ src/08simpleOCR.cpp
@@ -51,6 +51,7 @@ std::vector< std::vector <double > > analyzeContours(vector< vector< Point > > c
 		temp.push_back(area);
 		temp.push_back(perimeter);
 		temp.push_back(mu.mu12);
+		temp.push_back(mu.mu11);
 
 		output.push_back(temp);
 	}
@@ -78,64 +79,72 @@ int main( int argc, char** argv )
 	output = analyzeContours(findContoursInFile("../numbers/8.png"), 8, output);
 	output = analyzeContours(findContoursInFile("../numbers/9.png"), 9, output);
 
-	cv::Mat trainingDataLabels(output.size(), 1, CV_32SC1);
+	cv::Mat trainingDataLabels(output.size(), 1, CV_32FC1);
 	cv::Mat trainingData(output.size(), output.at(0).size() - 3, CV_32FC1);
 
 	for (size_t i = 0; i < output.size(); i++)
 	{   
-	//	trainingDataLabels.at<float>(i, 0) = output[i][0];
-		//printf("%8.3f\t ", output[i][0]);
+		trainingDataLabels.at<float>(i, 0) = output[i][0];
 	    	for (size_t j = 3; j < output.at(0).size(); j++)
 	    	{   
-	//		trainingData.at<float>(i, j - 3) = output[i][j];
-			//printf("%8.3f\t ", output[i][j]);
+			trainingData.at<float>(i, j - 3) = output[i][j];
 	    	}   
-		//printf("\n");
 	}   
+
+	std::cout << "trainingData = "<< std::endl << " "  << trainingData << std::endl << std::endl;
 
 	// Set up SVM's parameters
 	CvSVMParams params;
 	params.svm_type= CvSVM::C_SVC;
-	params.kernel_type = CvSVM::LINEAR;
+	params.kernel_type = CvSVM::POLY;
 	params.term_crit   = cvTermCriteria(CV_TERMCRIT_ITER, 100, 1e-6);
+	params.degree      = 2;
 	
 	// Train the SVM
 	CvSVM SVM;
 	printf("trainingData size: %d x %d\n", trainingData.rows, trainingData.cols);
 	printf("trainingDataLabels size: %d x %d\n", trainingDataLabels.rows, trainingDataLabels.cols);
-	//SVM.train(trainingData, trainingDataLabels, Mat(), Mat(), params);
+	SVM.train(trainingData, trainingDataLabels, Mat(), Mat(), params);
 
 	Mat image;
 	image = imread("../sudokuer/01bw.png", CV_LOAD_IMAGE_COLOR);
 
 	printf("Locating contours in test image.\n");
 	vector< vector< Point > > contours = findContoursInFile("../sudokuer/01bw.png");
-	
+
+	output = analyzeContours(contours, 1);
+
+	// Get feature descriptors.
+	cv::Mat objectData(output.size(), output.at(0).size() - 3, CV_32FC1);
+	for (size_t i = 0; i < output.size(); i++)
+	{   
+	    	for (size_t j = 3; j < output.at(0).size(); j++)
+	    	{   
+			objectData.at<float>(i, j - 3) = output[i][j];
+	    	}   
+	}   
+
 	// Start random number generator with a known seed
 	RNG rng(12345);
 
 	/// Draw contours
 	Mat drawing = Mat::zeros( image.size(), CV_8UC3 );
-	for( int i = 0; i< contours.size(); i++ )
+	for( int i = 0; i < output.size(); i++ )
 	{
-		// Calculate perimeter length
-		double perimeter = arcLength(contours[i], 1);
-		double area = contourArea(contours[i], true);
-		double compactness = perimeter * perimeter / (4 * 3.141592 * area);
-
-		if(area > 0)
+		float area = output[i][3];
+		if(area > 400)
 		{
-			//printf("perimeter: %8.3f  area: %8.3f   compactness: %8.3f\n", perimeter, area, compactness);
-
-			/// Get the moments
-			Moments mu;
-			mu = moments(contours[i], false); 
-			double hu[7];
-			cv::HuMoments(mu, hu); 
-
+			int coordx = output[i][1];
+			int coordy = output[i][2];
+			Mat sampleMat = (Mat_<float>(1, 4) << output[i][3], output[i][4], output[i][5], output[i][6]);
+			float recognizedClass = SVM.predict(sampleMat);
+			printf("%5d, %5d, %2f\n", coordx, coordy, recognizedClass);
 			// Draw contour
 			Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-			drawContours( image, contours, i, color, 3, 8);
+			circle(image, Point(coordx, coordy), 10, color, -1);
+			char str[200];
+			sprintf(str,"%.0f", recognizedClass);
+			putText(image, str, Point2f(coordx, coordy), FONT_HERSHEY_PLAIN, 4,  Scalar(120,120,120));
 		}
 	}
 	printf("Training completed.\n");
@@ -144,15 +153,5 @@ int main( int argc, char** argv )
 	imshow( "Contours", image);
 	
 	waitKey(0);
-
-
-	// Use the SVM
-//	float testData[1][2] = { {601, 10} };
-//	Mat testDataMat(1, 2, CV_32FC1, testData);
-//        float response = SVM.predict(testDataMat);
-
-//	printf("Response: %5.3f\n", response);
-
-
 }
 
